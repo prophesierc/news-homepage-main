b5d82e07981c3cc4910edfc017120762580b256e
ProphesierC <bwknowles98@gmail.com>
2024-06-20 22:39:47 -0400

    update index.html, scripts/index.js and styles/index.css main navbar animation work

--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,6 @@ class Page
     {
         this.init()
     }
-    
-    init()
-    {
-        this.modal()
-    }
 
     modal()
     {        
@@ -18,13 +13,25 @@ class Page
 
         openedIcon.onclick = () => 
         {
-            modal.showModal();
+            modal.show();
         }
+
         closedIcon.onclick = () => 
-        {
-            modal.close();
+        { 
+            modal.setAttribute("closing", ""); // creates empty attribute after initial click                      
+
+            modal.onanimationend = () => 
+            {
+                modal.removeAttribute("closing"); // removes attribute immediately after clicking
+                modal.close();
+                modal.onanimationend = null; // remove the event handler after each animationend to allow the event to rerun
+            };          
         }
+          
     }
-    
+    init()
+    {
+        this.modal()
+    }
 }
 const page = new Page();
